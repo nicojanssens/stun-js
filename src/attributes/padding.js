a05c412b0bf413b41b4d3eafe097fcd276d578e2
Nico Janssens <nico.b.janssens@gmail.com>
2016-06-07 12:05:13 +0200

    minor update of padding logic

--- src/attributes/padding.js
+++ src/attributes/padding.js
@@ -2,10 +2,12 @@
 
 exports.getBytes = getBytes
 
-var PADDING_VALUE = '0'
+var PADDING_VALUE = '0x00'
 
 function getBytes (length) {
   var paddingBytes = new Buffer((4 - length % 4) % 4)
-  paddingBytes.fill(PADDING_VALUE)
+  for (var i = 0; i < paddingBytes.length; i++) {
+    paddingBytes[i] = PADDING_VALUE
+  }
   return paddingBytes
 }
