3def7ab5e9a4737f664a3df05523acda674bd48c
Nico Janssens <nico.b.janssens@gmail.com>
2016-10-22 16:38:10 +0200

    retransmitting dropped stun requests while using UDP

--- test/stun_client.unit.js
+++ test/stun_client.unit.js
@@ -16,7 +16,7 @@ chai.should()
 var stunAddr = process.env.STUN_ADDR
 var stunPort = process.env.STUN_PORT
 
-var socketPort = 10000
+var socketPort = 20000
 
 describe('#STUN operations', function () {
   this.timeout(10000)
@@ -71,6 +71,37 @@ describe('#STUN operations', function () {
     socket.bind(socketPort)
   })
 
+  it('should execute STUN bind operation over UDP socket using promises + un-existing server', function (done) {
+    var retransmissionTimer
+    // send a STUN bind request and verify the reply
+    var sendBindRequest = function (client, socket) {
+      client.bindP()
+        .then(function () {
+          done(new Error('promise should not resolve'))
+        })
+        .catch(function (error) {
+          expect(error).not.to.be.undefined
+          done()
+        })
+    }
+    // create socket
+    var socket = dgram.createSocket('udp4')
+    socket.on('message', function (message, rinfo) { //
+      done(new Error('message callback should not be fired'))
+    })
+    socket.on('error', function (error) {
+      done(error)
+    })
+    socket.on('listening', function () {
+      // create stun client and pass socket over
+      var transport = new transports.UDP(socket)
+      var client = new StunClient('1.2.3.4', stunPort, transport)
+      // bind request
+      sendBindRequest(client, socket)
+    })
+    socket.bind(socketPort)
+  })
+
   it('should execute STUN bind operation over TCP socket using callbacks', function (done) {
     var transport = new transports.TCP()
     var client = new StunClient(stunAddr, stunPort, transport)
@@ -91,6 +122,27 @@ describe('#STUN operations', function () {
     // execute bind operation
     client.bind(onBindSuccess, onFailure)
   })
+
+  // it('should execute STUN bind operation over TCP socket using callbacks + un-existing server', function (done) {
+  //   var transport = new transports.TCP()
+  //   var client = new StunClient('1.2.3.4', stunPort, transport)
+  //   // if something fails
+  //   var onFailure = function (error) {
+  //     done(error)
+  //   }
+  //   // check bind results
+  //   var onBindSuccess = function (mappedAddress) {
+  //     expect(mappedAddress).not.to.be.undefined
+  //     expect(mappedAddress).to.have.property('address')
+  //     expect(mappedAddress).to.have.property('port')
+  //     // expect(mappedAddress.address).to.equal(testGW)
+  //     client.close(function () {
+  //       done()
+  //     })
+  //   }
+  //   // execute bind operation
+  //   client.bind(onBindSuccess, onFailure)
+  // })
 
   it('should execute STUN bind operation over unspecified UDP socket using promises', function (done) {
     var retransmissionTimer
